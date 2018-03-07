156ee65ed25aa06a3e2e53eb7b956185a6bf1937
Brad Coughlin <bradjcoughlin@gmail.com>
2018-03-07 11:57:52 -0800

    link symphony logo to main website

--- resources/assets/js/components/Navigation.vue
+++ resources/assets/js/components/Navigation.vue
@@ -11,7 +11,7 @@
         <span class="icon-bar"></span>
       </button>
 
-      <a class="navbar-brand sym-scroll" href="/"></a>
+      <a class="navbar-brand sym-scroll" href="https://symphony.com"></a>
       <img src="/images/symphony-logo-nav-light.svg" style="display: none;">
 
     </div>
