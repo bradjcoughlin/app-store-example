0e3b2c04b0760dfb9dbff8849491e18d6435f727
Brad Coughlin <bradjcoughlin@gmail.com>
2018-03-08 09:09:19 -0800

    add nav links

--- resources/assets/js/components/Navigation.vue
+++ resources/assets/js/components/Navigation.vue
@@ -23,48 +23,48 @@
         <li class="dropdown">
           <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Product <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Messaging and Productivity</a></li>
-            <li><a href="#">Security and Compliance</a></li>
-            <li><a href="#">App Directory</a></li>
-            <li><a href="#">Download</a></li>
-            <li><a href="#">Pricing</a></li>
+            <li><a href="https://symphony.com/product/messaging-and-productivity">Messaging and Productivity</a></li>
+            <li><a href="https://symphony.com/product/security-and-compliance">Security and Compliance</a></li>
+            <li><a href="/#/">App Directory</a></li>
+            <li><a href="https://symphony.com/download">Download</a></li>
+            <li><a href="https://symphony.com/product/pricing">Pricing</a></li>
           </ul>
         </li>
 
         <li class="dropdown">
           <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Solutions <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Financial Services</a></li>
-            <li><a href="#">&rsaquo;&nbsp;&nbsp;&nbsp;Sales &amp; Trading</a></li>
-            <li><a href="#">&rsaquo;&nbsp;&nbsp;&nbsp;Analysts</a></li>
-            <li><a href="#">&rsaquo;&nbsp;&nbsp;&nbsp;Operations</a></li>
-            <li><a href="#">&rsaquo;&nbsp;&nbsp;&nbsp;Portfolio Managers</a></li>
-            <li><a href="#">&rsaquo;&nbsp;&nbsp;&nbsp;Executives</a></li>
-            <li><a href="#">&rsaquo;&nbsp;&nbsp;&nbsp;Legal</a></li>
-            <li><a href="#">&rsaquo;&nbsp;&nbsp;&nbsp;Hedge Funds</a></li>
-            <li><a href="#">Insurance</a></li>
+            <li><a href="https://symphony.com/solutions">Financial Services</a></li>
+            <li><a href="https://symphony.com/solutions/financial-services/sales-and-trading">&rsaquo;&nbsp;&nbsp;&nbsp;Sales &amp; Trading</a></li>
+            <li><a href="https://symphony.com/solutions/financial-services/analysts">&rsaquo;&nbsp;&nbsp;&nbsp;Analysts</a></li>
+            <li><a href="https://symphony.com/solutions/financial-services/operations">&rsaquo;&nbsp;&nbsp;&nbsp;Operations</a></li>
+            <li><a href="https://symphony.com/solutions/financial-services/portfolio-managers">&rsaquo;&nbsp;&nbsp;&nbsp;Portfolio Managers</a></li>
+            <li><a href="https://symphony.com/solutions/financial-services/executives">&rsaquo;&nbsp;&nbsp;&nbsp;Executives</a></li>
+            <li><a href="https://symphony.com/solutions/financial-services/legal">&rsaquo;&nbsp;&nbsp;&nbsp;Legal</a></li>
+            <li><a href="https://symphony.com/solutions/financial-services/hedge-funds">&rsaquo;&nbsp;&nbsp;&nbsp;Hedge Funds</a></li>
+            <li><a href="https://symphony.com/solutions/insurance">&rsaquo;&nbsp;&nbsp;&nbsp;Insurance</a></li>
           </ul>
         </li>
         
         <li class="dropdown">
           <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Company <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Press</a></li>
-            <li><a href="/events">Events</a></li>
-            <li><a href="#">Organization</a></li>
-            <li><a href="#">Careers</a></li>
-            <li><a href="#">Vision</a></li>
-            <li><a href="#">Contact Us</a></li>
+            <li><a href="https://symphony.com/press/news">Press</a></li>
+            <li><a href="https://symphony.com/events">Events</a></li>
+            <li><a href="https://symphony.com/company/organization">Organization</a></li>
+            <li><a href="https://symphony.com/company/careers">Careers</a></li>
+            <li><a href="https://symphony.com/company/innovation-award">Innovation Award</a></li>
+            <li><a href="https://symphony.com/contact-us">Contact Us</a></li>
           </ul>
         </li>
 
-        <li><a href="/blog">Blog</a></li>
+        <li><a href="https://symphony.com/blog">Blog</a></li>
 
-        <li><a href="#" data-sym-event-action="Clicked Sign In" data-sym-event-category="External Links">Sign In</a></li>
+        <li><a href="https://symphony.com/sign-in">Sign In</a></li>
 
         <li id="sym-nav-signup">
           <div class="btn-group btn-block">
-            <a href="#" class="btn btn-block btn-default-ghost" data-sym-event-action="Clicked Sign Up (Top Nav)" data-sym-event-category="External Links">Try it Now</a>
+            <a href="https://symphony.com/signup" class="btn btn-block btn-default-ghost">Try it Now</a>
           </div>
         </li>
 
