35d664d4323376e83edfb245995e72df96aae863
Brad Coughlin <bradjcoughlin@gmail.com>
2018-03-09 09:38:32 -0800

    filter with radio buttons

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -17,7 +17,38 @@
                 <div class="row">
                     <h3>Apps, Bots, &amp; Integrations</h3>
                     <p>Facilitate, enable, and simplify daily workflows on Symphony using bots, integrations, and partner applications. Take a look at these innovations made available via Symphony’s end-to-end encrypted infrastructure and open APIs.</p>
-                    <card v-for="entry in entries"
+
+                </div>
+
+                <div class="row sym-form">
+                    <form class="form-inline">
+                        <div class="form-group">
+                            <input id="all" type="radio" v-model="selectedType" value="All" />
+                            <label for="all">All Types</label>
+                        </div>
+                        <div class="form-group">
+                            <input id="extension" type="radio" v-model="selectedType" value="Extension App" />
+                            <label for="extension">Extension App</label>
+                        </div>
+                        <div class="form-group">
+                            <input id="integration" type="radio" v-model="selectedType" value="Integration" />
+                            <label for="integration">Integration</label>
+                        </div>
+                        <div class="form-group">
+                            <input id="chatbot" type="radio" v-model="selectedType" value="Chat Bot" />
+                            <label for="chatbot">Chat Bot</label>
+                        </div>
+                        <div class="form-group">
+                            <input id="embedded" type="radio" v-model="selectedType" value="Embedded Chat" />
+                            <label for="embedded">Embedded Chat</label>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="row">
+                    
+
+                    <card v-for="entry in filteredEntries"
                         :key="entry.id"
                         v-bind:entry="entry"></card>
                 </div>
@@ -39,16 +70,32 @@
                 entries: [],
                 loading: true,
                 loaded: false,
-                pageYOffset: 0
+                pageYOffset: 0,
+                selectedType: 'All'
+            }
+        },
+        computed: {
+            filteredEntries: function() {
+                //var vm = this;
+                var type = this.selectedType;
+                
+                if(type === 'All') {
+                    return this.entries;
+                } else {
+                    return this.entries.filter(function(entry) {
+                        return entry.type === type;
+                    });
+                }
             }
         },
         created() {
             axios.get('/api/entries')
                 .then(response => {
                   // JSON responses are automatically parsed.
-                  this.entries = response.data
+                  this.entries = response.data;
                   this.loading = false;
                   this.loaded = true;
+                  (document.documentElement||document.body).scrollTop = 0;
                 })
                 .catch(e => {
                   this.errors.push(e)
@@ -57,7 +104,7 @@
         },
         updated: function() {
             // scroll page to top
-            (document.documentElement||document.body).scrollTop = 0
+            //(document.documentElement||document.body).scrollTop = 0
         },
         methods: {
             // no methods
@@ -97,6 +144,17 @@
 p {
     text-align: center;
     max-width: 960px;
-    margin: 0 auto 12px auto;
+    margin: 0 auto 24px auto;
+}
+.row.sym-form {
+    text-align: center;
+}
+.form-inline {
+    display: inline-block;
+    margin: 0 auto 12px 0;
+}
+label {
+    font-weight: 500;
+    margin-right: 16px;
 }
 </style>
