8a8be5d6a4a5a7029e640700ce8c3d9019f7b3ba
Brad Coughlin <bradjcoughlin@gmail.com>
2018-04-23 10:13:33 -0700

    add additional partners
    nikkei etc

--- resources/assets/js/components/Partners.vue
+++ resources/assets/js/components/Partners.vue
@@ -11,45 +11,99 @@
         <div class="col-sm-4">
             <a href="http://www.bluefireai.com/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="/images/blue-fire-ai.png"></a>
         </div>
+        <!-- Chart IQ -->
+        <div class="col-sm-4">
+            <a href="https://www.chartiq.com/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/web/logo/partner-logos/1/ChartIQ.png"></a>
+        </div>
+        <!-- Dow Jones -->
+        <div class="col-sm-4">
+            <a href="https://www.dowjones.com/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/web/logo/partner-logos/DowJones.png"></a>
+        </div>
+        <!-- Factset -->
         <div class="col-sm-4">
             <a href="https://www.factset.com/analytics/collaboration-and-messaging" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/partners/factset.png"></a>
         </div>
+        <!-- FinTech Studios -->
+        <div class="col-sm-4">
+            <a href="https://apollo.fintechstudios.com/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/web/logo/partner-logos/1/FinTechStudios.png"></a>
+        </div>
+        <!-- FIS Global -->
         <div class="col-sm-4">
             <a href="http://www.fisglobal.com/Solutions/Institutional-and-Wholesale/Commercial-and-Investment-Banking/Trading-and-Connectivity#marketdata" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/partners/fis-global.png"></a>
         </div>
+        <!-- Flextrade -->
         <div class="col-sm-4">
             <a href="https://flextrade.com/flextrade-integrates-symphonys-secure-and-compliant-communications/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/partners/flextrade.png"></a>
         </div>
+        <!-- Harvest Exchange -->
+        <div class="col-sm-4">
+            <a href="https://info.hvst.com/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/web/logo/partner-logos/HarvestExchange.png"></a>
+        </div>
+        <!-- Infront -->
+        <div class="col-sm-4">
+            <a href="https://www.infrontanalytics.com/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/web/logo/partner-logos/InfrontAnalytics.png"></a>
+        </div>
+        <!-- ipushpull -->
+        <div class="col-sm-4">
+            <a href="https://www.ipushpull.com/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="/images/ipushpull.png"></a>
+        </div>
+        <!-- IHS Markit -->
         <div class="col-sm-4">
             <a href="https://ihsmarkit.com/products.html" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/partners/ihs-markit.png"></a>
         </div>
+        <!-- Koyfin -->
         <div class="col-sm-4">
             <a href="https://koyfin.com/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/partners/koyfin.png"></a>
         </div>
+        <!-- Lodestar -->
         <div class="col-sm-4">
             <a href="http://www.lodestar-ecosystems.com/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/partners/lodestar.png"></a>
         </div>
+        <!-- Earlybird -->
         <div class="col-sm-4">
             <a href="https://www.marketearlybird.com/symphony" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/partners/market-earlybird.png"></a>
         </div>
+        <!-- MDX Technology -->
         <div class="col-sm-4">
             <a href="http://www.mdxtechnology.com/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/partners/mdx-technology.png"></a>
         </div>
+        <!-- Money.net -->
         <div class="col-sm-4">
             <a href="https://www.money.net" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/partners/moneynet.png"></a>
         </div>
+        <!-- Neptune -->
         <div class="col-sm-4">
             <a href="http://neptunefi.com/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/partners/neptune.png"></a>
         </div>
+        <!-- NexJ -->
+        <div class="col-sm-4">
+            <a href="http://www.nexj.com/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/web/logo/partner-logos/NexJ.png"></a>
+        </div>
+        <!-- Nikkei -->
+        <div class="col-sm-4">
+            <a href="http://www.nikkei.co.jp/nikkeiinfo/en/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="/images/nikkei.png"></a>
+        </div>
+        <!-- Revista Systems -->
         <div class="col-sm-4">
             <a href="https://revistasystems.com/Solutions" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/partners/revista-systems.png"></a>
         </div>
+        <!-- RSRCHXchange -->
         <div class="col-sm-4">
             <a href="http://www.rsrchxchange.com/about/collaborations.html" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/partners/rsrch-xchange.png"></a>
         </div>
-        <div class="col-sm-4" style="margin-left: 16.6666666666%">
+        <!-- SmartKarma -->
+        <div class="col-sm-4">
             <a href="https://www.smartkarma.com/home/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/partners/smartkarma.png"></a>
         </div>
+         <!-- S&P Global -->
+        <div class="col-sm-4">
+            <a href="https://marketintelligence.spglobal.com/" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/web/logo/partner-logos/S&P.png"></a>
+        </div>
+        <!-- Selerity -->
+        <div class="col-sm-4">
+            <a href="http://www.seleritycorp.com/selerity-context.html" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/web/logo/partner-logos/Selerity.png"></a>
+        </div>
+        <!-- Tick42 -->
         <div class="col-sm-4">
             <a href="https://glue42.com" target="_blank" rel="nofollow noopener noreferrer"><img class="img-responsive" src="https://symphony.com/images/partners/tick42.png"></a>
         </div>
